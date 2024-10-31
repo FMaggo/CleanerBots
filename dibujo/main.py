5b683828892059370745ed27f77d4a60b0cc9afe
Diego <diegosoltri9@gmail.com>
2024-10-31 16:48:56 -0600

    Visualizacionrobotcaja

---
+++ dibujo/main.py
@@ -0,0 +1,77 @@
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+import sys
+import requests
+from OpMat import OpMat
+from caja import Caja
+
+# URL base de la API y obtención de datos
+URL_BASE = "http://localhost:8000"
+payload = {"dim": [40, 40]}  # Define la dimensión esperada en el servidor
+r = requests.post(URL_BASE + "/simulations", json=payload, allow_redirects=False)
+datos = r.json()
+print("Datos iniciales:", datos)
+
+LOCATION = datos["Location"]
+
+# Dimensiones del plano y límites de ejes
+X_MIN, X_MAX = -450, 450
+Y_MIN, Y_MAX = -450, 450
+
+# Inicialización de objetos
+opmat = OpMat()
+cajas_normales = []  # Lista de cajas que no son robots
+robots = []          # Lista de cajas que representan robots
+
+# Crear instancias de Caja y Robot según los datos recibidos
+for box_data in datos["boxes"]:
+    caja_obj = Caja(opmat)
+    x_pos = box_data["pos"][0] * 20 - 400
+    y_pos = box_data["pos"][1] * 20 - 400
+    caja_obj.update_position(x_pos, y_pos)
+    cajas_normales.append(caja_obj)
+
+for robot_data in datos["robots"]:
+    robot_obj = Caja(opmat)  # Usamos la clase Caja para representar a los robots
+    x_pos = robot_data["pos"][0] * 20 - 400
+    y_pos = robot_data["pos"][1] * 20 - 400
+    robot_obj.update_position(x_pos, y_pos)
+    robots.append(robot_obj)
+
+def init():
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluOrtho2D(X_MIN, X_MAX, Y_MIN, Y_MAX)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+    glClearColor(0.0, 0.0, 0.0, 0.0)
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+
+def display():
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+    # Dibujar todas las cajas normales en blanco
+    for caja_obj in cajas_normales:
+        caja_obj.render()  # Blanco por defecto
+
+    # Dibujar los robots en rojo
+    for robot_obj in robots:
+        robot_obj.render(color=(1.0, 0.0, 0.0))  # Rojo
+
+    glutSwapBuffers()
+
+def timer(value):
+    glutPostRedisplay()
+    glutTimerFunc(16, timer, 0)  # Aproximadamente 60 FPS
+
+if __name__ == "__main__":
+    glutInit(sys.argv)
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB)
+    glutInitWindowSize(600, 600)
+    glutInitWindowPosition(100, 100)
+    glutCreateWindow("Robots y Cajas")
+    init()
+    glutDisplayFunc(display)
+    glutTimerFunc(0, timer, 0)
+    glutMainLoop()
